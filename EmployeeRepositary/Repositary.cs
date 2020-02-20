ad153c21d737c9626e48611124188fb0e04938bc
Tamilarasi <Tamilarasi@DESKTOP-7VRA19A>
2020-02-20 22:42:12 +0530

    Validation_MVC

--- EmployeeRepositary/Repositary.cs
+++ EmployeeRepositary/Repositary.cs
@@ -10,8 +10,7 @@ namespace EmployeeRepositary
     {
         public static List<Employee> employees = new List<Employee>();
         static Repositary()
-        {
-           
+        {          
             //employees.Add(new Employee {firstName= "Tamil",lastName= "arasi", id=1, "female", "tamil@gmail.com", 9787617628, Convert.ToDateTime("12/12/1995"), Convert.ToDateTime("12/12/2020"), "Tamil", "tamil@123", (Designation)Enum.Parse(typeof(Designation), "HR") });
             employees.Add(new Employee { firstName = "Tamil", lastName = "arasi", id = "1", emailId = "tamil@gmail.com", gender = "female", mobileNumber = "9787617628", dob ="12/12/1995", doj = "12/12/2020", userName = "Tamil", password = "tamil@123",designation=(Designation)Enum.Parse(typeof(Designation), "HR") });
         }
